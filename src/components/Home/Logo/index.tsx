40df378ab1092cec338fcae3be9542f7ed6e206a
geraldkachi <fitzgeraldkachi@gmail.com>
2022-04-03 05:23:49 +0100

    finish

--- src/components/Home/Logo/index.tsx
+++ src/components/Home/Logo/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react"
 import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+// import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoS from '../../../assets/images/logo-s.png'
 import "./index.scss"
 const Logo = () => {
@@ -10,7 +10,7 @@ const Logo = () => {
     
 
     useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
+        gsap.registerPlugin()
 
         gsap
             .timeline()
